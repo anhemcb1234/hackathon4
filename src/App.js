6b1e22eeee1a65dcc25f1c2244af616ae5fe1650
thuan-nv035 <54436704+thuan-nv035@users.noreply.github.com>
2022-05-10 15:36:24 +0700

    login

--- src/App.js
+++ src/App.js
@@ -6,14 +6,15 @@ import {
   Link,
 } from "react-router-dom";
 import About from './components/About';
+import Login from './components/Login';
 export default function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<About />} >
+          <Route path="/" element={<About />} />
+          <Route path='/login' element = {<Login/>} />
         
-          </Route>
 
         </Routes>
         {/* <Link to={`/about?name=mien&age=20`}>about</Link> */}
